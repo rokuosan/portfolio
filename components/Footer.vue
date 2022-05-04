a11a1664c8f83555641cc758902e951ac99d3f75
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-05-05 00:48:19 +0900

    Add Footer

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="fixed py-3 w-full bottom-0 bg-gray-800 text-gray-50">
+    <nav class="w-11/12 mx-auto text-3xl">
+      <h6 class="font-raleway">
+        Sample Footer
+      </h6>
+    </nav>
+  </div>
+</template>
