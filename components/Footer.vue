43cdf75a3400ad68c4c9f03dfb868998a73d3791
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-05-05 02:33:29 +0900

    著作権表示を追加

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="fixed py-3 w-full bottom-0 bg-gray-800 text-gray-50">
-    <nav class="w-11/12 mx-auto text-3xl">
-      <h6 class="font-raleway">
-        Sample Footer
+  <div class="fixed py-3 w-full bottom-0">
+    <nav class="w-11/12 mx-auto text-md">
+      <h6 class="font-poppins font-light">
+        Copyright Ⓒ 2022 rokuosan , All rights reserved.
       </h6>
     </nav>
   </div>
