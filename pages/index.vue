56036f90fb9c5f965a98eca5588034bac8fda4ee
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-05-05 02:42:15 +0900

    Profile を表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="pt-20 text-4xl w-full">
-    <div class="w-5/6 mx-auto">
-      <h1>
-        This is sample text.
-      </h1>
-      <NuxtLink to="about">
-        Go to About
-      </NuxtLink>
-    </div>
-  </div>
+  <Profile />
 </template>
 
 <script>
@@ -16,3 +7,6 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style>
+</style>
