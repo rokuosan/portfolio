d778a021a5250cf2b3ef5eb194f2078c576eaba0
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-05-06 00:47:00 +0900

    フッターを調整

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="fixed py-3 w-full bottom-0">
-    <nav class="w-11/12 mx-auto text-md">
-      <h6 class="font-poppins font-light">
+  <div class="py-3 absolute bottom-0 right-0">
+    <nav class="text-sm w-fit">
+      <h6 class="font-poppins font-light text-neutral-400">
         Copyright Ⓒ 2022 rokuosan , All rights reserved.
       </h6>
     </nav>
