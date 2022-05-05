a6a3e5d28ea6b75809e6d94065b65f1090ebb29e
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-05-05 18:37:30 +0900

    スタイルを修正

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,39 +1,28 @@
 <template>
-  <div class="py-20 grid grid-cols-1 md:grid-cols-3 md:gap-8 gap-y-8 md:w-5/6 w-[95%] mx-auto font-poppins">
-    <div class="col-span-1 md:justify-self-end justify-self-center flex relative select-none">
-      <img width="256" height="256" alt="Avatar" src="https://avatars.githubusercontent.com/u/85651386?v=4" class="rounded-full border-4 border-sky-900">
-    </div>
-    <div class="col-span-2">
-      <h3 class="text-4xl border-sky-900 border-b-2">
-        Profile
-      </h3>
-      <div class="grid grid-cols-3 text-2xl pt-5 gap-x-6">
-        <div class="col-span-1 key grid grid-cols-1 gap-y-4 text-center font-light">
-          <div>
-            Name
+  <div class="w-full bg-indigo-50">
+    <div class="py-20 grid grid-cols-1 md:grid-cols-3 md:gap-8 gap-y-8 md:w-5/6 w-[95%] mx-auto font-poppins relative text-indigo-700 md:border-l-2 border-indigo-700">
+      <div class="absolute origin-bottom-left -rotate-90 text-4xl bottom-5 left-0 text-indigo-700 md:visible invisible italic">
+        Self Introduction
+      </div>
+      <div class="col-span-1 md:justify-self-end justify-self-center flex relative select-none">
+        <img alt="Avatar" src="https://avatars.githubusercontent.com/u/85651386?v=4" class="self-center rounded-full bg-in lg:w-64 lg:h-64 w-48 h-48">
+      </div>
+      <div class="col-span-2">
+        <h3 class="text-4xl border-indigo-400 border-b-2">
+          Profile
+        </h3>
+        <div class="grid grid-cols-3 text-2xl pt-5 gap-x-6">
+          <div class="col-span-1 key grid grid-cols-1 gap-y-4 text-center font-light">
+            <div> Name </div>
+            <div> Birthday </div>
+            <div> Country </div>
+            <div> Favorite </div>
           </div>
-          <div>
-            Birthday
-          </div>
-          <div>
-            Country
-          </div>
-          <div>
-            Favorite
-          </div>
-        </div>
-        <div class="col-span-2 value grid grid-cols-1 gap-y-4 md:text-left text-center">
-          <div>
-            rokuosan
-          </div>
-          <div>
-            03/03
-          </div>
-          <div>
-            Japan
-          </div>
-          <div>
-            Kotlin
+          <div class="col-span-2 value grid grid-cols-1 gap-y-4 md:text-left text-center">
+            <div> rokuosan </div>
+            <div> 03/03 </div>
+            <div> Japan </div>
+            <div> Kotlin </div>
           </div>
         </div>
       </div>
@@ -46,3 +35,9 @@ export default {
   name: 'Profile'
 }
 </script>
+
+<style scoped>
+body{
+  background: rgb(224, 231, 255);
+}
+</style>
