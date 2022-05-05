351534b1d6e4136faf568ba09d165a32c543b068
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-05-06 00:46:45 +0900

    WorkとCardコンポーネントを追加

--- components/Work.vue
+++ components/Work.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="w-full bg-neutral-50">
-    <div class="py-20 md:w-5/6 w-[95%] mx-auto relative md:border-l-4 border-neutral-900">
+  <div class="w-full">
+    <div class="py-20 md:w-5/6 w-[95%] mx-auto relative md:border-l-4 border-neutral-600">
       <!--   Vertical Border   -->
-      <div class="absolute origin-bottom-left -rotate-90 text-4xl bottom-5 left-0 md:visible invisible italic select-none text-neutral-900">
+      <div class="absolute origin-bottom-left -rotate-90 text-4xl bottom-5 left-0 md:visible invisible italic select-none text-neutral-600">
+        Works
+      </div>
+      <!--  Title    -->
+      <div class="md:w-48 text-4xl border-b-2 border-indigo-500 text-center rounded mb-8 mx-auto">
         Works
       </div>
       <!--  Cards    -->
-      <div class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 text-neutral-900 px-8">
-        <div class="border border-neutral-500 col-span-1">
-          <h4 class="text-3xl">Teleport Assist</h4>
-          <a href="https://github.com/rokuosan/teleport-assist" target="_blank" ref="noopener noreferrer">View on GitHub</a>
-        </div>
-        <div class="border border-neutral-500 col-span-1">
-          <h4 class="text-3xl">Paper Downloader</h4>
-          <a href="https://github.com/rokuosan/paper-downloader-nuxt2" target="_blank" ref="noopener noreferrer">View on GitHub</a>
-        </div>
-        <div class="border border-neutral-500 col-span-1">
-          <h4 class="text-3xl">Inspect Horse</h4>
-          <a href="https://github.com/rokuosan/inspecthorse" target="_blank" ref="noopener noreferrer">View on GitHub</a>
-        </div>
-        <div class="border border-neutral-500 col-span-1">
-          <h4 class="text-3xl">The half of One Hundred Poems</h4>
-          <a href="https://github.com/rokuosan/onehundredpoems" target="_blank" ref="noopener noreferrer">View on GitHub</a>
-        </div>
+      <div class="grid 2xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-8 px-8 justify-items-center">
+        <Card title="Teleport Assist" description="プレイヤーにインタラクティブなテレポート手段を新たに提供するプラグイン。"
+              :tags="['Minecraft', 'Plugin']" link="https://github.com/rokuosan/teleport-assist"/>
+        <Card title="Paper Downloader" description="Paper MC をバージョン指定してダウンロード可能な簡易インターフェース。"
+              :tags="['Minecraft', 'Application']" link="https://github.com/rokuosan/paper-downloader-nuxt2"/>
+        <Card title="Inspect Horse" description="馬の速度と跳躍力のパラメータが取得できるコマンドを追加するプラグイン。"
+              :tags="['Minecraft', 'Plugin']" link="https://github.com/rokuosan/inspecthorse"/>
+        <Card title="The half of One Hundred Poems" description="百人一首の下50首のみを最短で覚えるためのWebアプリケーション"
+              :tags="['Application', 'Fun']" link="https://github.com/rokuosan/onehundredpoems"/>
+        <Card title="Kotlin Fullstack...? Slide"
+              description="Kotlin/JS で躓いてKotlinのフルスタックアプリケーションに失敗した話をLT向けにまとめたスライド。"
+              :tags="['Slide', 'Fun']" link="https://speakerdeck.com/rokuosan/kotlinwoshi-tutemaikuraqing-woi"/>
       </div>
     </div>
   </div>
