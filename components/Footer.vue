70ec4ce46707b7dd109dc7431dcc55f103fa6adf
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-12-29 01:45:32 +0900

    Change framework

--- components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-3 w-full bg-neutral-900 border-t-4 border-neutral-600">
-    <nav class="text-sm">
-      <h6 class="font-poppins font-light text-neutral-400">
-        Copyright Ⓒ 2022 rokuosan , All rights reserved.
-      </h6>
-    </nav>
-  </div>
-</template>
